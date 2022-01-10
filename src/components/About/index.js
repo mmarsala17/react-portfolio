b735dfff81c23b562d11e4f67fe12448f774e186
Mark Marsala <mmarsala76@gmail.com>
2022-01-10 13:48:38 -0500

    about me updates

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,17 @@ import React from "react";
 function About() {
     return (
         <section>
-            <h1 id="about">About Me</h1>
+     <div className="center page-header" id="about">
+         About Me
+     </div>
+     <div className="center">
+         <img
+            src={require('../../assets/images/mark and syd.jpg')}
+            alt="Mark and Syd"
+            className="photo"
+            />
+     </div>
+     <div>
             <p>
                 Hi! My name is Mark Marsala and I know that you've come here to see my work and learn about me. I was born in Ft. Myers, FL and I currently live in Charlotte, NC with my wife and my son Enzo. durring the day I take care of my son and during the night I play competitive video games. currently ive been competing in <em>Halo</em>. 
             </p>
@@ -13,6 +23,7 @@ function About() {
             <p>
                 One of the reasons i decided to take this class is that i enjoy coding and want to build a career out of it. 
             </p>
+            </div>
         </section>
     );
 }
